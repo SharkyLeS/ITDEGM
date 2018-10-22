b001929c3c3190f48e5ad09027bfae03e1467712
guillaume <guillaume@GS-Thinkpad>
2018-10-22 12:10:58 +0200

    Fourmis VIE

---
+++ src/tsp/Colonie.java
@@ -0,0 +1,76 @@
+package tsp;
+
+public abstract class Colonie {
+	private double alpha; //pondere les phéromones
+	private double beta; //pondere la visibilité
+	private double rho; //
+	private double Q;
+	private double[][] visibilite;
+	private double[][] pheromones; // les tau (i,j)
+	private Instance instance;
+
+	public Colonie() {
+	}
+	
+	public Colonie(Instance m_instance,double alph, double bet, double rh, double q) throws Exception {
+		this.instance = m_instance;
+		this.alpha = alph;
+		this.beta = bet;
+		this.rho = rh;
+		this.Q=q;
+		this.pheromones = new double[instance.getNbCities()][instance.getNbCities()];
+		visibilite = new double[instance.getNbCities()][instance.getNbCities()];
+		
+		// initialise les matrices
+		for (int i=0; i<instance.getNbCities(); i++) {
+			for (int j=i+1; j<instance.getNbCities(); j++) {
+				
+				this.visibilite[i][j] = 1/(instance.getDistances(i, j));
+				this.visibilite[j][i] = this.visibilite[i][j];
+				
+				this.pheromones[i][j] = 0;
+				this.pheromones[j][i] = 0;
+			}
+			this.visibilite[i][i] = 0;
+			this.pheromones[i][i] = 0;
+		}
+	}
+	
+	public Instance getInstance() {
+		return this.instance;
+	}
+
+	public double getAlpha() {
+		return alpha;
+	}
+
+	public double getBeta() {
+		return beta;
+	}
+
+	public double getRho() {
+		return rho;
+	}
+
+	public double[][] getVisibilite() {
+		return visibilite;
+	}
+
+	// ne pas priviliégier ce getter
+	public double[][] getPheromones() {
+		return pheromones;
+	}
+	
+	public double getPheromones(int i,int j) {
+		return pheromones[i][j];
+	}
+
+	public void setPheromones(double[][] pheromones) {
+		this.pheromones = pheromones;
+	}
+	
+	public void setPheromones(double valeur, int i, int j) {
+		this.pheromones[i][j] = valeur;
+	}
+	
+}
--- src/tsp/Fourmi.java
+++ src/tsp/Fourmi.java
@@ -1,5 +1,52 @@
 package tsp;
 
-public class Fourmi {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Fourmi extends Colonie {
+	private ArrayList<Integer> villesParcourues;
+	private Double[][] proba; //Matrice symétrique des probabilités, partiellement mise à jour à chaque étape
+	private ArrayList<Integer> villesRestantes;
+	
+	public Fourmi(int villeDeDepart) {
+		super(); // A VOIR
+		this.villesParcourues = new ArrayList<Integer>();
+		this.proba = new Double[super.getInstance().getNbCities()][super.getInstance().getNbCities()];
+		this.villesRestantes = new ArrayList<Integer>();
+		for(int i=0;i<super.getInstance().getNbCities();i++) {
+			if (i != villeDeDepart) {
+				this.villesRestantes.add(i);
+			}
+		}
+	}
+	
+	//Met à jour this.proba autour de i ; tourne avant de choisir une ville
+	public void majProba() {
+		
+	}
+	
+	/*avec le math.random et les probas
+	 * Comme la somme des probas vaut 1, on fait des intervalles en sommant (voir dessin)
+	 * Est-ce que ce sera exactement ? -> on prend le dernier
+	 * 
+	 * + MET A JOUR VILLES RESTANTES ET VILLES PARCOURUES*/
+	public void choixVille() {
+		
+	}
+	
+	//Renvoie la longueur totale parcourue (somme des trajets)
+	public int getLongueur() {
+		return 0;
+	}
+	
+	//Renvoie la quantité supplémentaire de phéromones (delta tau) à déposer sur tous les arcs empruntés
+	public double getDeltaPheromones() {
+		return 0.0;
+	}
+	
+	//utilise setPheromones pour modifier super.pheromones
+	public void majPheromones() {
+		// rho * ancienne valeur + delta tau
+	}
+	
 }
