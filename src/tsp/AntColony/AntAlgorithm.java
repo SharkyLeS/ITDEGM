1639c9b595c52d0fcf9b97247ae5a4f1eb9c37fa
guillaume <guillaume@GS-Thinkpad>
2018-11-02 17:47:17 +0100

    avec m fourmis

--- src/tsp/AntColony/AntAlgorithm.java
+++ src/tsp/AntColony/AntAlgorithm.java
@@ -1,7 +1,6 @@
 package tsp.AntColony;
 
 import java.util.ArrayList;
-
 import tsp.Instance;
 import tsp.Solution;
 import tsp.metaheuristic.AMetaheuristic;
@@ -9,9 +8,10 @@ import tsp.metaheuristic.AMetaheuristic;
 public class AntAlgorithm extends AMetaheuristic {
 
 	public static final boolean trace = false;
-	public static final double alpha = 1.0; //pondere les phéromones A DEFINIR
-	public static final double beta = 5.0; //pondere la visibilité
-	public static final double rho = 0.5; // doit être entre 0 et 1
+	public static final int m = 49; // Nombre de fourmis par cycle
+	public static final double alpha = 1.0; // pondere les phéromones A DEFINIR
+	public static final double beta = 5.0; // pondere la visibilité
+	public static final double rho = 0.7; // doit être entre 0 et 1
 	public static final double Q = 100.0;
 
 	
@@ -34,8 +34,8 @@ public class AntAlgorithm extends AMetaheuristic {
 				this.visibilite[j][i] = this.visibilite[i][j];
 				//if (trace) System.out.println("distance entre " + i + " et " + j +" : "+instance.getDistances(i, j));
 				//if (trace) System.out.println("visibilité entre " + i + " et " + j +" : "+this.visibilite[i][j]);
-				this.pheromones[i][j] = 1;
-				this.pheromones[j][i] = 1;
+				this.pheromones[i][j] = 100;
+				this.pheromones[j][i] = 100;
 			}
 			this.visibilite[i][i] = 0;
 			this.pheromones[i][i] = 0;
@@ -116,30 +116,26 @@ public class AntAlgorithm extends AMetaheuristic {
 		return villeChoisie;
 	}
 	
-	
-	public String toStringMatrice(double[][] mat) {
-		String s = "[";
-		for (int i=0; i<mat.length; i++) {
-			s+= "[";
-			for (int j=0; j<mat[0].length; j++) {
-				s+= mat[i][j] + " , ";
-			}
-			s+= "] ," + "\n";
-		}
-		return s;
-	}
-	
 	public double getDeltaPheromones(double longueur) throws Exception { //Max
 		return Q/longueur;
 	}
 	
-	public void majPheromones(Solution s, double pheromone, double longeur) throws Exception { /*Ajout Max */
+	public void majPheromones(ArrayList<Solution> listeSolution, double pheromones) throws Exception { /*Ajout Max */
 		for (int i=0;i<super.getInstance().getNbCities()-1;i++) {
-			this.setPheromones(rho*this.getPheromones(s.getCity(i),s.getCity(i+1))+
-					(pheromone + this.getDeltaPheromones(longeur)), s.getCity(i), s.getCity(i+1)) ;
+			for (int j=i+1;j<super.getInstance().getNbCities()-1;j++) {
+				this.setPheromones(rho*this.getPheromones(i,j),i,j); 
+				this.setPheromones(rho*this.getPheromones(j,i),j,i); 
+			}
+		}
+		
+		for (Solution s : listeSolution) {
+			for (int i=0; i<super.getInstance().getNbCities();i++) {
+				this.setPheromones(this.getPheromones(s.getCity(i), s.getCity(i+1)) + pheromones, s.getCity(i), s.getCity(i+1));
+			}
 		}
 	}
-		public Solution lanceFourmi(int villeDeDepart, double pheromone) throws Exception {
+	
+		public Solution lanceFourmi(int villeDeDepart) throws Exception {
 			Solution sol = new Solution(super.getInstance());
 			// Initialisation
 			sol.setCityPosition(villeDeDepart, 0);
@@ -161,44 +157,36 @@ public class AntAlgorithm extends AMetaheuristic {
 					} /* Obliger de recopier la fonction lance fourmi, sinon, fonctions qui s'apellent sans cesse entre elle */
 				}		
 			}
-		majPheromones(sol, pheromone, sol.evaluate());
 		return sol;
-	}
-
-	public Solution lanceFourmi() throws Exception {
-		return lanceFourmi(0,0);
 	}
 
 	public Solution solve(Solution sol,long time) throws Exception {
+		// this.bestSol = sol.copy(); déjà fait dans l'initialisation !
 		long startTime = System.currentTimeMillis();
-		/* Avant : int i=0; */
+		ArrayList<Solution> solList = new ArrayList<Solution>();
 		Solution solActuelle;
-		Solution solActuelle1;
-		Solution solActuelle2;
-		Solution solActuelle3;
-		Solution solActuelle4;
-		Solution solActuelle5;
-		double pheromone=0;
-		solActuelle1 = lanceFourmi((int) Math.random()*101,0);
-		solActuelle2 = lanceFourmi((int) Math.random()*101,0);
-		solActuelle3 = lanceFourmi((int) Math.random()*101,0);
-		solActuelle4 = lanceFourmi((int) Math.random()*101,0);
-		solActuelle5 = lanceFourmi((int) Math.random()*101,0);
-		pheromone = getDeltaPheromones(solActuelle1.evaluate())+getDeltaPheromones(solActuelle2.evaluate())+getDeltaPheromones(solActuelle3.evaluate())+getDeltaPheromones(solActuelle4.evaluate())+getDeltaPheromones(solActuelle5.evaluate());
+		double deltaPheromone = 0.0;
+		int compteur = 0;
+		int villeDeDepart = super.getInstance().getNbCities()-1 ;
+		
 		while (System.currentTimeMillis() - startTime<time*1000-100) {
-			/* Avant : solActuelle = lanceFourmi(i); */
-			solActuelle = lanceFourmi((int) Math.random()*101, pheromone);
-			System.out.println(solActuelle.getObjectiveValue());
-			System.out.println("------------------------------------------");
-			if (solActuelle.getObjectiveValue()<sol.getObjectiveValue()) {
-				sol = solActuelle.copy();
+			solActuelle = lanceFourmi(villeDeDepart);
+			System.out.println(solActuelle.evaluate());
+			solList.add(solActuelle);
+			deltaPheromone += getDeltaPheromones(solActuelle.getObjectiveValue());
+			villeDeDepart = (int) (Math.random()*(super.getInstance().getNbCities()));
+			compteur++;
+
+			if (solActuelle.getObjectiveValue()<bestSol.getObjectiveValue()) this.bestSol = solActuelle.copy();
+
+			if (compteur == m) {
+				majPheromones(solList,deltaPheromone);
+				solList = new ArrayList<Solution>();
+				deltaPheromone = 0.0;
+				compteur =0;
 			}
 		}
-		
-		return sol;
+		return this.bestSol;
 	}
 
 } 
-/**/
-
-
